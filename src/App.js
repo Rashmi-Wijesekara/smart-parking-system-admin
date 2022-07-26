2f40c7a0d0248c8d809df641f99adbaba67168e5
Laksisi Gunaratne <chethagunaratne@gmail.comchethagunaratne@gmail.com>
2022-07-26 10:36:28 +0530

    Created app.js and auth.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from "react-router-dom";
+
+import "./App.css";
+import {ProtectedRoute} from './ProtectedRoute'
+
+
+import ParkingLog from "./dashboard/pages/ParkingLog";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="font-bold text-red-500 text-xl">hello world</div>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<ProtectedRoute
+					exact
+					path="/parking-log"
+					component={ParkingLog}
+				/>
+				
+				<Route path="/error">404 not found</Route>
+				<Redirect to="/error" />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
